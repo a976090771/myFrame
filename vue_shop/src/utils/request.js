9644055d7ee688872152b7aa8bde8de8c36bddbe
zt131 <9270706+zt131@user.noreply.gitee.com>
2022-08-15 21:54:48 +0800

    20220825提交

--- vue_shop/src/utils/request.js
+++ vue_shop/src/utils/request.js
@@ -3,6 +3,7 @@ axios.defaults.baseURL = "http://timemeetyou.com:8889/api/private/v1/"
 
 axios.interceptors.request.use(config=>{
    config.headers.Authorization = sessionStorage.getItem("token")
+   console.log("20220825")
    return config
 })
 
